da6a91f704722c7cce88c14b3f5f782a2d4b8c44
chrisosma24 <101012681+chrisosma24@users.noreply.github.com>
2023-03-17 16:17:02 -0400

    Update HelloApplication.java

--- HelloApplication.java
+++ HelloApplication.java
@@ -7,7 +7,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Driver class to run project 3
+ * @author Christian Osma, Liam Smith
+ */
 public class HelloApplication extends Application {
+    /**
+     * Overriden method that is the main entry point for the application
+     * @param stage Stage that is to be displayed on screen
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -18,6 +26,10 @@ public class HelloApplication extends Application {
         stage.setResizable(false);
     }
 
+    /**
+     * This method is responsible for starting up the JavaFX application
+     * @param args array of Strings
+     */
     public static void main(String[] args) {
         launch();
     }
